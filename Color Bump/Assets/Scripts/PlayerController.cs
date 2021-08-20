b55d9be6d054b5786f6cb9846f1c6b3af0cabbd6
HuseyinZorbeyZengin <huseyinzorbey34@gmail.com>
2021-08-20 03:46:54 +0300

    Project Files

--- Color Bump/Assets/Scripts/PlayerController.cs
+++ Color Bump/Assets/Scripts/PlayerController.cs
@@ -18,6 +18,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 vector = lastMousePos - Input.mousePosition;
+            lastMousePos = Input.mousePosition;
+            vector = new Vector3(vector.x, 0, vector.y);
 
+            Vector3 moveForce = Vector3.ClampMagnitude(vector, clampDelta);
+            rb.AddForce(Vector3.forward * 2 + (-moveForce * sensitivity - rb.velocity / 5f), ForceMode.VelocityChange);
+        }
     }
 }
